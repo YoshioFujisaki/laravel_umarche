d3b9a5c14efe913bed3f255bab99981a1108ca1e
YoshioFujisaki <a27879038@gmail.com>
2023-08-01 17:30:06 +0900

    product 初期設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Shop;
+use App\Models\SecondaryCategory;
+use App\Models\Image;
+
 class Product extends Model
 {
     use HasFactory;

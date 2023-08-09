1dda67723cd94117ea4aeabad1f4e4d7ec04521c
YoshioFujisaki <a27879038@gmail.com>
2023-08-09 22:23:33 +0900

    cart add

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -67,19 +67,23 @@
                                         number_format($product->price) }}</span>
                                     <span class="text-sm text-gray-700">円(税込み)</span>
                                 </div>
-                                <div class="flex items-center">
-                                    <span class="mr-3">数量</span>
-                                    <div class="relative">
-                                        <select name="quantity"
-                                            class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                                            @for ($i = 1; $i <= $quantity; $i++)
-                                            <option value="{{ $i }}">{{$i}}</option>
-                                            @endfor
-                                        </select>
+                                <form action="{{ route('user.cart.add') }}" method="post">
+                                    @csrf
+                                    <div class="flex items-center">
+                                        <span class="mr-3">数量</span>
+                                        <div class="relative">
+                                            <select name="quantity"
+                                                class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                                                @for ($i = 1; $i <= $quantity; $i++) <option value="{{ $i }}">{{$i}}
+                                                    </option>
+                                                    @endfor
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <button
-                                    class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    <button
+                                        class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">カートに入れる</button>
+                                    <input type="hidden" name="product_id" value="{{ $product->id}}">
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -108,7 +112,7 @@
             <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
                 <header class="modal__header">
                     <h2 class="text-xl text-gray-700" id="modal-1-title">
-                        {{ $product->shop->name  }}
+                        {{ $product->shop->name }}
                     </h2>
                     <button type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
                 </header>
